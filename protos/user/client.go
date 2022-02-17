2a80039cbef38b1cf0a78403734d9bb34baf8c86
Nyx <Nyx@swit.io>
2022-02-17 16:21:33 +0900

    test

--- protos/user/client.go
+++ protos/user/client.go
@@ -6,19 +6,19 @@ import (
 
 // Namespace : version
 // Copy 이후 환경 변수명 체크 필.
-func getGRPCClientConn()  {
+func getGRPCClientConn() {
 
 	env.GetString("USER_SVC")
 	/*
-	a := client.Address{
-		IP:        env.GetString("USER_SVC"),
-		Namespace: env.GetString("NS_V4"),
-		Port:      env.GetString("USER_PORT"),
-	}
-	conn := client.NewGrpcClientProvider(a)
-		*/
+		a := client.Address{
+			IP:        env.GetString("USER_SVC"),
+			Namespace: env.GetString("NS_V4"),
+			Port:      env.GetString("USER_PORT"),
+		}
+		conn := client.NewGrpcClientProvider(a)
+
+		return conn
 	*/
-	return conn
 }
 
 /*
